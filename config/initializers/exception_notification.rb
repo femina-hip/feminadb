6a2da48d54dda146a0f3e4d72bbadf5dd26cb8d5
Adam Hooper <adam@adamhooper.com>
2010-03-06 12:18:03 -0500

    Fix exception_notification config

--- config/initializers/exception_notification.rb
+++
@@ -1,5 +0,0 @@
-Feminadb::Application.config.middleware.use(ExceptionNotifier,
-  :email_prefix => '[feminadb] ',
-  :sender_address => 'FeminaDB <adam@adamhooper.com>',
-  :exception_recipients => %w{adam@adamhooper.com}
-)
