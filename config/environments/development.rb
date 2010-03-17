b7a742fb9a03209e38e6566b1933a4e467c0a771
Adam <adam@adamhooper.com>
2010-03-16 23:45:48 -0400

    Remove backtrace silencers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,6 @@ Feminadb::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
+  Rails.backtrace_cleaner.remove_silencers!
 end
