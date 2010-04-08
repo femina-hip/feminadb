773e4452a661eb3b2091ac81ac90ac03ebb747f2
Adam Hooper <adam@adamhooper.com>
2010-04-08 19:00:22 -0400

    Fix pagination to work with html_safe

--- config/initializers/pagination_renderer.rb
+++ config/initializers/pagination_renderer.rb
@@ -1,2 +1,1 @@
-require 'pagination/renderer'
 WillPaginate::ViewHelpers.pagination_options[:renderer] = Pagination::Renderer
--- lib/pagination/renderer.rb
+++ lib/pagination/renderer.rb
@@ -1,32 +1,29 @@
 class Pagination::Renderer < WillPaginate::LinkRenderer
   def to_html
-    stuff = []
-    stuff << 'Page'
-    stuff << page_form
-    stuff << 'of %d' % send(:total_pages)
+    inner = "Page #{page_form} of #{send(:total_pages)}".html_safe
 
-    @template.content_tag(:div, stuff, html_attributes)
+    @template.content_tag(:div, inner, html_attributes)
   end
 
   private
-    def hidden_inputs
-      options = @template.params.dup
-      options.delete :page
-      options.delete :commit
-      options.delete :controller
-      options.delete :action
-      options.merge(@options[:params]) if @options[:params]
 
-      options.collect{ |k,v| @template.hidden_field_tag k, v }
-    end
+  def hidden_inputs
+    options = @template.params.dup
+    options.delete :page
+    options.delete :commit
+    options.delete :controller
+    options.delete :action
+    options.merge(@options[:params]) if @options[:params]
 
-    def page_form
-      @template.content_tag(:form, @template.url_for) do
-        @template.content_tag(:div) do
-          hidden_inputs + [
-            @template.text_field_tag(:page, send(:current_page))
-          ]
-        end
-      end
+    options.collect{ |k,v| @template.hidden_field_tag(k, v) }
+  end
+
+  def page_form
+    stuff = hidden_inputs + [@template.text_field_tag(:page, send(:current_page))]
+    inner = stuff.join.html_safe
+
+    @template.content_tag(:form, @template.url_for) do
+      @template.content_tag(:div, inner)
     end
+  end
 end
