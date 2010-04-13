b2f90246d61538357ed280f14a41e17f4e8f3f09
Adam Hooper <adam@adamhooper.com>
2010-04-13 17:50:32 +0300

    Hide most of Customer page by default

---
+++ app/javascripts/customers/_customer.js
@@ -0,0 +1,20 @@
+$NS().find('thead.delivery-details, thead.contact-details').each(function() {
+  var $thead = $(this);
+  var $tbody = $(this).next();
+
+  $tbody.hide();
+
+  var $a = $('<a class="show-hide">Show</a>');
+  $thead.find('th').append(' ');
+  $thead.find('th').append($a);
+
+  $a.click(function() {
+    $tbody.slideToggle(function() {
+      if ($tbody.is(':visible')) {
+        $a.text('Hide');
+      } else {
+        $a.text('Show');
+      }
+    });
+  });
+});
---
+++ app/javascripts/customers/show.js
@@ -0,0 +1,20 @@
+$NS().filter('table:not(.customer-notes)').find('thead.title').each(function() {
+  var $thead = $(this);
+  var $siblings = $thead.siblings();
+
+  $siblings.hide();
+
+  var $a = $('<a class="show-hide">Show</a>');
+  $thead.find('th').append(' ');
+  $thead.find('th').append($a);
+
+  $a.click(function() {
+    $siblings.slideToggle(function() {
+      if ($siblings.is(':visible')) {
+        $a.text('Hide');
+      } else {
+        $a.text('Show');
+      }
+    });
+  });
+});
