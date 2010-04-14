c5db503d6ae70ce0dadbe03b5273094cc899fe67
Adam Hooper <adam@adamhooper.com>
2010-04-14 11:45:40 +0300

    Fix deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,13 @@ module Feminadb
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password
+
+    config.session_store(
+      :cookie_store,
+      :key    => '_feminadb_session',
+      :secret => '92ba21b35fb78673b7207957cbf83236e77388b2a460bbce935e3ea86baa0fdaffcbc8ef4d92f8c7da805f202e6a717b0e32dce119f50a6a050265b8cd94e9cf'
+    )
+
+    config.cookie_secret = 'd307f8ef2f161c536015ba537fae2f06a8342e322d5ab0bc93dfe80ddf23703bd39b17330d0d23ab7f700bef7388c4fc2ca4a373a4b86cec5a7fd86281d45a02'
   end
 end
