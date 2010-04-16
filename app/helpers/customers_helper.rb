587d44597c25e1b3350fcfb4721a6e33ece90861
Adam Hooper <adam@adamhooper.com>
2010-04-16 12:36:18 +0300

    Don't double-escape URLs in customer info

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,16 +1,16 @@
 module CustomersHelper
   def telephones_string(customer)
-    h [ customer.telephone_1, customer.telephone_2, customer.telephone_3 ].collect{|ct| ct.to_s.strip}.select{|ct| not ct.empty?}.join(', ')
+    [ customer.telephone_1, customer.telephone_2, customer.telephone_3 ].collect{|ct| ct.to_s.strip}.select{|ct| not ct.empty?}.join(', ')
   end
 
   def emails_string(customer)
-    [ customer.email_1, customer.email_2 ].collect{|e| e.to_s.strip}.select{|e| not e.empty?}.collect{|e| mail_to(e)}.join(', ')
+    [ customer.email_1, customer.email_2 ].collect{|e| e.to_s.strip}.reject(&:empty?).collect{|e| mail_to(e)}.join(', ').html_safe
   end
 
   def website_string(customer)
     url = customer.website.to_s.strip
     return '' if url.empty?
     url = "http://#{url}" unless url =~ /^https?:/
-    link_to url, url, :popup => true
+    link_to(url)
   end
 end
