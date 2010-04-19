2003cd9d9c0bf5cd27fa66cfe2044c2c54262133
Adam Hooper <adam@adamhooper.com>
2010-04-19 09:24:32 +0300

    Eliminate one more query on Customers index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -26,7 +26,7 @@ class CustomersController < ApplicationController
 
     @customers = search.results
     Customer.send(:preload_associations, @customers, [:region, :type, :standing_orders, :waiting_orders])
-    @publications = Publication.tracking_standing_orders.active.order(:name).all
+    @publications = Publication.active.tracking_standing_orders.order(:name).all
 
     respond_to do |type|
       type.html do
