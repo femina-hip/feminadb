a4aecd1d245cb08c249a4964abb217f3db01d5f7
Adam Hooper <adam@adamhooper.com>
2010-04-29 15:28:02 +0300

    Reorder tracking_standing_orders

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -22,7 +22,8 @@ class Publication < ActiveRecord::Base
   validates_uniqueness_of :name, :scope => :deleted_at, :if => lambda { |p| p.deleted_at.nil? }
 
   scope :tracking_standing_orders,
-        :conditions => { :tracks_standing_orders => true }
+        :conditions => { :tracks_standing_orders => true, :deleted_at => nil },
+        :order => :name
 
   scope :current_periodicals,
         :conditions => { :tracks_standing_orders => true, :deleted_at => nil },
