8c1d1093b70fe8afba69fa0e6f313627adc91397
Adam Hooper <adam@adamhooper.com>
2010-05-04 17:33:16 +0200

    Put Help on Customer searching

--- app/helpers/helps_helper.rb
+++ app/helpers/helps_helper.rb
@@ -6,4 +6,8 @@ module HelpsHelper
   def render_help_step(title, &block)
     content_tag(:li, (content_tag(:h3, title) + capture(&block)).html_safe)
   end
+
+  def search_link(search)
+    link_to(search, customers_path(:q => search))
+  end
 end
