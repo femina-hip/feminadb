4848add710503b5fb4fa9b5ad7291496ee9527c6
Adam Hooper <adam@adamhooper.com>
2010-07-20 12:44:01 +0300

    Describe IDs of CustomerType, etc in "values" changes; style fixes

--- app/helpers/modifications_helper.rb
+++ app/helpers/modifications_helper.rb
@@ -16,38 +16,35 @@ module ModificationsHelper
     link_to('history', modifications_path(:versioned_type => record.class.model_name, :versioned_id => record.id), :class => 'history')
   end
 
-  def show_attribute(record, column)
-    case column
-      when 'customer_id' then format_customer_id(record)
-      when 'updated_by' then format_updated_by(record)
-      when 'customer_type_id' then format_customer_type_id(record)
-      when 'delivery_method_id' then format_delivery_method_id(record)
-      when 'region_id' then format_region_id(record)
-      else h record.send(column).to_s
+  def format_column_value(column_name, value)
+    case column_name
+      when 'customer_id' then format_customer_id(value)
+      when 'customer_type_id' then format_customer_type_id(value)
+      when 'delivery_method_id' then format_delivery_method_id(value)
+      when 'region_id' then format_region_id(value)
+      else value
     end
   end
 
   private
-    def format_customer_id(m)
-      h Customer.find_with_deleted(m.customer_id).name
-    end
 
-    def format_updated_by(m)
-      'hello'
-    end
+  def format_customer_id(id)
+    c = Customer.find(id)
+    c && "#{id} (#{c.name})" || id
+  end
 
-    def format_customer_type_id(m)
-      ct = CustomerType.find_with_deleted(m.customer_type_id)
-      h "#{ct.name} - #{ct.description}"
-    end
+  def format_customer_type_id(id)
+    ct = CustomerType.find(id)
+    ct && "#{id} (#{ct.name} - #{ct.description})" || id
+  end
 
-    def format_delivery_method_id(m)
-      dm = DeliveryMethod.find(m.delivery_method_id)
-      h "#{dm.abbreviation} - #{dm.name}"
-    end
+  def format_delivery_method_id(id)
+    dm = DeliveryMethod.find(id)
+    dm && "#{id} (#{dm.abbreviation} - #{dm.name})" || id
+  end
 
-    def format_region_id(m)
-      r = Region.find_with_deleted(m.region_id)
-      h "#{r.name}"
-    end
+  def format_region_id(id)
+    r = Region.find(id)
+    r && "#{id} (#{r.name})" || id
+  end
 end
