880b81b49fd3731a3ac222ec0d6d27411cc0e3cb
Adam Hooper <adam@adamhooper.com>
2010-07-23 15:39:37 +0300

    Show a friendly error message when DixMax parsing fails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
 
   layout 'default'
   require_role 'view'
+
+  rescue_from(RSolr::RequestError, :with => :render_rsolr_error)
+
+  private
+
+  def render_rsolr_error(exception)
+    render(:text => "Sorry, your search didn't work, but it's not your fault nor that of your coworkers! The \"Solr\" people are likely responsible. Please try searching for something else and tell Adam Hooper, <a href=\"mailto:adam@adamhooper.com\">adam@adamhooper.com</a>, what you were searching for when you got this error message. (Just copy-paste the contents of the address bar; that will be enough.)".html_safe)
+  end
 end
