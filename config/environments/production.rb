bf10951c4bfc40c1d0c9bb46fb8bd55f6f70eebf
Adam Hooper <adam@adamhooper.com>
2015-09-29 14:44:49 +0300

    Silence deprecation warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,10 @@ Feminadb::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_files = false
   config.assets.compile = false
   config.assets.digest = true
   config.assets.js_compressor = :uglifier
   config.eager_load = true
+  config.log_level = :info
 end
