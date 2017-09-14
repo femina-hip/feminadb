65ccfbd749a7417dddf1ab1c504a6f591e9393ec
Adam Hooper <adam@adamhooper.com>
2017-09-14 15:19:33 +0300

    Just count Headmaster Telerivet contacts

--- app/models/reports.rb
+++ app/models/reports.rb
@@ -47,10 +47,10 @@ module Reports
           regions.population,
           SUM(schools.n_fema) AS n_fema,
           regions.population / SUM(schools.n_fema) AS population_per_fema,
-          SUM(schools.has_telerivet) AS n_schools_with_telerivet,
-          100 * SUM(schools.has_telerivet) / COUNT(*) AS percent_schools_with_telerivet,
-          SUM(schools.has_fema * schools.has_telerivet) AS n_schools_with_fema_and_telerivet,
-          100.0 * SUM(schools.has_fema * schools.has_telerivet) / SUM(schools.has_fema) AS percent_fema_schools_with_telerivet,
+          SUM(schools.has_headmaster_telerivet) AS n_schools_with_headmaster_telerivet,
+          100 * SUM(schools.has_headmaster_telerivet) / COUNT(*) AS percent_schools_with_headmaster_telerivet,
+          SUM(schools.has_fema * schools.has_headmaster_telerivet) AS n_schools_with_fema_and_headmaster_telerivet,
+          100.0 * SUM(schools.has_fema * schools.has_headmaster_telerivet) / SUM(schools.has_fema) AS percent_fema_schools_with_headmaster_telerivet,
           SUM(schools.has_club) AS n_schools_with_club,
           100.0 * SUM(schools.has_club) / COUNT(*) AS percent_schools_with_club,
           100.0 * SUM(schools.has_club) / SUM(schools.has_fema) AS percent_fema_schools_with_club
@@ -70,11 +70,10 @@ module Reports
               WHERE customer_id = customers.id
               AND publication_id = (SELECT id FROM publications WHERE name = 'Fema')
           ), 0) AS n_fema,
-            LENGTH(REGEXP_REPLACE(CONCAT(primary_contact_sms_numbers, headmaster_sms_numbers, club_sms_numbers, student_sms_numbers), '[^+]', '')) AS n_telerivet,
-          INSTR(CONCAT(primary_contact_sms_numbers, headmaster_sms_numbers, club_sms_numbers, student_sms_numbers), '+') > 0 AS has_telerivet,
-          INSTR(club_sms_numbers, '+') AS has_club
+          INSTR(headmaster_sms_numbers, '+') > 0 AS has_headmaster_telerivet,
+          INSTR(club_sms_numbers, '+') > 0 AS has_club
           FROM customers
-          WHERE customer_type_id IN (SELECT id FROM customer_types WHERE category = 'Educational Institutions')
+          WHERE customer_type_id IN (SELECT id FROM customer_types WHERE name LIKE 'SS %')
         ) schools ON regions.id = schools.region_id
         GROUP BY regions.name, regions.manager, regions.population
         ORDER BY regions.name
