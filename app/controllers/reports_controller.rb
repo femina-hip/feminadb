8e47a54767c085e55be5ee53d28429cb3970a1e1
Adam Hooper <adam@adamhooper.com>
2017-09-14 14:28:22 +0300

    Export to CSV

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,6 @@
 class ReportsController < ApplicationController
+  respond_to(:csv, :html)
+
   def show
     case params[:id]
     when 'staff-schools-clubs' then show_staff_schools_clubs
@@ -11,11 +13,31 @@ class ReportsController < ApplicationController
 
   def show_staff_schools_clubs
     @table = Reports.staff_schools_clubs
-    render('staff-schools-clubs')
+    respond('staff-schools-clubs')
   end
 
   def show_school_contacts
     @table = Reports.school_contacts
-    render('school-contacts')
+    respond('school-contacts')
+  end
+
+  # Either sends @table to render(view_name), or renders @table as a CSV.
+  def respond(view_name)
+    respond_with do |format|
+      format.html { render(view_name) }
+      format.csv { render_csv(view_name) }
+    end
+  end
+
+  def render_csv(view_name)
+    csv = CSV.generate(headers: true) do |out|
+      keys = @table[0].keys
+      out << keys
+
+      @table.each do |row|
+        out << keys.map { |key| row[key] }
+      end
+    end
+    send_data(csv, filename: "#{view_name}.csv")
   end
 end
