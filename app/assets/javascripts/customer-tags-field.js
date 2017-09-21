6dc8c64f7d29604d4758c8707eaf034b247436ef
Adam Hooper <adam@adamhooper.com>
2017-09-21 16:24:49 +0300

    Sort tags in customer-tags-field

--- app/assets/javascripts/customer-tags-field.js
+++ app/assets/javascripts/customer-tags-field.js
@@ -5,6 +5,7 @@
   if (!tagsJson) return
 
   var tags = JSON.parse(tagsJson) // Array of { id, name, color, light_or_dark }
+    .sort(function(a, b) { return a.name.localeCompare(b.name) })
 
   function renderTag(tag, escape) {
     if (!tag) tag = { id: 0, name: '(Unknown tag)', color: '#000000', light_or_dark: 'dark' }
@@ -15,6 +16,23 @@
 
   function makeTagSelect(div) {
     var customerId = parseFloat(div.getAttribute('data-customer-id'))
+
+    // build tagIds, a sorted, comma-separated list of IDs. Only include valid
+    // IDs.
+    //
+    // customers_tags has no order; when the user edits, we won't re-sort. The
+    // next time the page loads, this code will run again and so the list will
+    // be re-sorted for the user.
+    var tagIdsSet = {}
+    div.getAttribute('data-tag-ids')
+      .split(/, */g)
+      .filter(function(s) { return s.length > 0 })
+      .forEach(function(s) { tagIdsSet[s] = null })
+    var sortedTagIdsString = tags
+      .filter(function(tag) { return tagIdsSet.hasOwnProperty(String(tag.id)) })
+      .map(function(tag) { return tag.id })
+      .join(',')
+
     var nAjaxCalls = 0
 
     function tagCustomer(tagId) {
@@ -45,14 +63,14 @@
     }
 
     $('<input type="text" tabindex="-1" style="display:none" placeholder="Tags"/>')
-      .val(div.getAttribute('data-tag-ids'))
+      .val(sortedTagIdsString)
       .appendTo(div)
       .selectize({
         maxItems: null,
         hideSelected: true,
         options: tags,
         searchField: [ 'name' ],
-        sortField: [ { field: 'name' } ],
+        sortField: [ { field: 'name' }, { field: '$score' } ],
         valueField: 'id',
         render: {
           item: renderTag,
