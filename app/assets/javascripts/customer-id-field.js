664fdb1146bcb3b0d759ee12fe1d10586c961d80
Adam Hooper <adam@adamhooper.com>
2017-09-25 09:58:44 +0300

    Make customer-id-field work properly

--- app/assets/javascripts/customer-id-field.js
+++ app/assets/javascripts/customer-id-field.js
@@ -2,9 +2,9 @@ $.fn.customer_id_field = function() {
   const option_template = _.template([
     '<div class="customer-id-option">',
       '<span class="name"><%- name %></span>',
-      '<abbr class="type" title="<%- type.description %>"><%- type.name %></abbr>',
+      '<span class="type"><%- type.name %></span>',
       '<span class="address">',
-        '<span class="council"><%- council %></span>, <span class="region"><%- region %></span>',
+        '<span class="region"><%- region %></span><span class="council"><%- council %></span>',
       '</span>',
     '</div>'
   ].join(''))
@@ -12,15 +12,19 @@ $.fn.customer_id_field = function() {
   function one_customer_id_field(_, el) {
     $(el).selectize({
       valueField: 'id',
-      labelField: 'name',
-      searchField: '',
+      highlight: false,
+      openOnFocus: true,
+      searchField: [],
       create: false,
       render: {
         item: option_template,
         option: option_template
       },
+      loadThrottle: 150, // Selectize default is a painful 300
+      score: function() { return function() { return 1 } }, // all ajax results are matches
       load: function(query, callback) {
         if (!query.length) return callback()
+        var _this = this
 
         $.ajax({
           url: '/customers/autocomplete?q=' + encodeURIComponent(query),
@@ -29,6 +33,7 @@ $.fn.customer_id_field = function() {
             callback()
           },
           success: function(data) {
+            _this.clearOptions() // https://github.com/selectize/selectize.js/issues/1053
             callback(data)
           }
         })
