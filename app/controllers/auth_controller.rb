331595682b8daef2086621317860efde4e6c2108
Adam Hooper <adam@adamhooper.com>
2021-03-06 13:34:50 -0500

    Auto-create user accounts on first login
    If a user is authorized by Google Apps, that means they're allowed
    to access FeminaDB as a viewer.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,13 +2,10 @@ class AuthController < ActionController::Base
   def callback
     oauth_response = OauthStrategy.ask_google_about_code(request)
     email = oauth_response['email']
-    if User.find_by_email(email)
-      destination = params[:state] || '/'
-      session[:user_email] = email
-      redirect_to(destination)
-    else
-      raise Exception, "User #{email} does not exist in FeminaDB. An administrator needs to add you."
-    end
+    User.find_or_create_by!(email: email)
+    destination = params[:state] || '/'
+    session[:user_email] = email
+    redirect_to(destination)
   end
 
   def logout
